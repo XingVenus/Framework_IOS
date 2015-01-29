21fd1985fca9ec09ca0d5ff3dc8f68a6ebf20f5f
Xing.Jin <venuslife@sina.cn>
2015-01-29 13:01:36 +0800

    出去玩首页

---
+++ HuWai/Classes/Views/GoOutPlay/CityList.h
@@ -0,0 +1,13 @@
+//
+//  CityList.h
+//  HuWai
+//
+//  Created by WmVenusMac on 15-1-29.
+//  Copyright (c) 2015年 xici. All rights reserved.
+//
+
+#import "BaseTableViewController.h"
+
+@interface CityList : BaseTableViewController
+
+@end
